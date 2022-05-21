3e1d68015243b1db4747b8b945a7c7ccb62d3e69
Crax97 <gsolimeno97@gmail.com>
2022-05-21 17:58:27 +0200

    add regex test for testing with case sensitive

--- src/tests.rs
+++ src/tests.rs
@@ -30,6 +30,19 @@ mod tests {
         #[test]
         fn test_find_matching_with_regex() {
             let config = Configuration {
+                case_insensitive: false,
+                pattern: Pattern::regex_from_string(r".?the.?")
+            };
+            let content = "The quick brown
+            fox jumps over
+            the lazy dog"; 
+            let matches = find_in_lines(content.split("\n"), &config);
+            assert_eq!(matches.len(), 1);
+        }
+        
+        #[test]
+        fn test_find_matching_with_regex_case_insensitive() {
+            let config = Configuration {
                 case_insensitive: true,
                 pattern: Pattern::regex_from_string(r".?the.?")
             };
