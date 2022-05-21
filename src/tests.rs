280c648b3d3937251603271adc823a04fbeaf5fa
Crax97 <gsolimeno97@gmail.com>
2022-05-21 17:56:49 +0200

    move regex_tests into mod tests

--- src/tests.rs
+++ src/tests.rs
@@ -22,22 +22,21 @@ mod tests {
         let matches = find_in_lines(content.split("\n"), &config);
         assert!(matches.len() == 2);
     }
-}
+    
+    #[cfg(feature = "regex")]
+    mod regex_tests {
+        use crate::fif::*;
 
-#[cfg(feature = "regex")]
-#[cfg(test)]
-mod regex_tests {
-    use crate::fif::*;
-
-    #[test]
-    fn test_find_matching_with_regex() {
-        let config = Configuration {
-            case_insensitive: true,
-            pattern: Pattern::regex_from_string(r".?the.?")
-        };
-        let content = "The quick brown
-        fox jumps over the lazy dog"; 
-        let matches = find_in_lines(content.split("\n"), &config);
-        assert_eq!(matches.len(), 2);
+        #[test]
+        fn test_find_matching_with_regex() {
+            let config = Configuration {
+                case_insensitive: true,
+                pattern: Pattern::regex_from_string(r".?the.?")
+            };
+            let content = "The quick brown
+            fox jumps over the lazy dog"; 
+            let matches = find_in_lines(content.split("\n"), &config);
+            assert_eq!(matches.len(), 2);
+        }
     }
 }
